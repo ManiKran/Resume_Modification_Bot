9929bf041f6f47bc596d2dd019fce92cb82c26f5
ManiKran <87695509+ManiKran@users.noreply.github.com>
2025-11-23 00:05:55 +0000

    Refactor CORS settings in main.py and update host permissions in manifest.json for improved extension compatibility

--- app/main.py
+++ app/main.py
@@ -2,17 +2,23 @@ from fastapi import FastAPI
 from sqlalchemy import text
 from app.db.database import Base, engine
 from app.routers.resume_router import router as resume_router
-from app.db.database import Base, engine
 from app.routers.optimize_router import router as optimize_router
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Resume AI Backend")
 
+# IMPORTANT:
+# Railway auto-redirects from http → https
+# And its domain may contain subdomains.
+allowed_extensions = [
+    "chrome-extension://mgbpoaobnhfajkaemeaalinanaecdpmi"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["chrome-extension://mgbpoaobnhfajkaemeaalinanaecdpmi",
-        "https://resumemodificationbot-production.up.railway.app"],
+    allow_origins=allowed_extensions,   # chrome extension direct origins
+    allow_origin_regex=r"https://.*\.railway\.app",  # ALLOW ALL HTTPS Railway URLs
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -21,6 +27,7 @@ app.add_middleware(
 @app.on_event("startup")
 def startup():
     Base.metadata.create_all(bind=engine)
+
 
 app.include_router(resume_router)
 app.include_router(optimize_router)
@@ -33,7 +40,6 @@ app.mount("/generated", StaticFiles(directory="generated"), name="generated")
 
 @app.get("/reset-db")
 def reset_db():
-    from app.db.database import Base, engine
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
     return {"status": "Database reset successfully"}
