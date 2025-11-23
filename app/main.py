7b9a2b74437728fad727db43b568141018ca9915
ManiKran <87695509+ManiKran@users.noreply.github.com>
2025-11-23 01:34:11 +0000

    changed the extension id

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ app = FastAPI(title="Resume AI Backend")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "chrome-extension://fgdjdccbdhnmniafodedgabomedoljoo",
+        "chrome-extension://mgbpoaobnhfajkaemeaalinanaecdpmi",
         "https://resumemodificationbot-production.up.railway.app",
         "http://resumemodificationbot-production.up.railway.app",
     ],
